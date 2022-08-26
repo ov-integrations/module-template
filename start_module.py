50f17247cbd78e60f25fd1f2af0996740a5784c5
asmoiseenko <asmoiseenko@users.noreply.github.com>
2022-08-26 15:28:30 +0500

    Fix file extension

--- start_module.py
+++ start_module.py
@@ -2,7 +2,7 @@ import sys
 import subprocess
 
 installed_dependencies = subprocess.check_output(
-    [sys.executable, '-m', 'pip', 'install', '-r', 'python_dependencies.txt']).decode().strip()
+    [sys.executable, '-m', 'pip', 'install', '-r', 'python_dependencies.ini']).decode().strip()
 if 'Successfully installed' in installed_dependencies:
     raise Exception('Some required dependent libraries were installed. ' \
         'Module execution has to be terminated now to use installed libraries on the next scheduled launch.')
