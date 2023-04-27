25899272639feaa640bbc479417637db7037d110
asmoiseenko <asmoiseenko@users.noreply.github.com>
2023-04-27 14:27:51 +0500

    Update 050.Rebuild-Objref.sql

--- installation/050.Rebuild-Objref.sql
+++ installation/050.Rebuild-Objref.sql
@@ -1,3 +1,9 @@
+--SQL to be executed during Module installation must be placed in the numbered *.sql files.
+--Each file can include a single PL/SQL block
+--PL/SQL block termination characters are not supported
+--PL/SQL will be executed in the _pkg DB schema
+--In case of error, module installation cannot proceed
+--DBMS Output will be shown in UI
 begin
     pkg_objref.rebuild_id_pkg_and_types();
     dbms_output.put_line('Object referencies were rebuilt');
