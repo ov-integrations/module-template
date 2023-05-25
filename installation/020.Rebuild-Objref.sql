aedbc245d70f9877f4e872549854184677af9812
asmoiseenko <asmoiseenko@users.noreply.github.com>
2023-05-25 18:41:28 +0500

    Update 020.Rebuild-Objref.sql

--- installation/020.Rebuild-Objref.sql
+++ installation/020.Rebuild-Objref.sql
@@ -7,6 +7,7 @@
 --All changes will be committed automatically on script completion
 
 begin
+    pkg_objref.validate_rebuild_id_pkg_and_types();
     pkg_objref.rebuild_id_pkg_and_types();
     dbms_output.put_line('Object referencies were rebuilt');
 end;
