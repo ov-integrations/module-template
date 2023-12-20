20b3695e134e22bc73ee32e2e7374e0c2ae2968d
Vadim <glebowadim@gmail.com>
2023-12-20 17:25:58 +0700

    Fix typo in 020.Rebuild-Objref.sql

--- installation/020.Rebuild-Objref.sql
+++ installation/020.Rebuild-Objref.sql
@@ -9,5 +9,5 @@
 begin
     pkg_objref.validate_rebuild_id_pkg_and_types();
     pkg_objref.rebuild_id_pkg_and_types();
-    dbms_output.put_line('Object referencies were rebuilt');
+    dbms_output.put_line('Object references were rebuilt');
 end;
