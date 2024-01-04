4c63aee946c5b2329f164fb3b24e2f0a111f8259
asmoiseenko <asmoiseenko@users.noreply.github.com>
2024-01-04 09:14:00 -0500

    Add sample code to activate automations

--- installation/040.Rules-Activation.sql
+++ installation/040.Rules-Activation.sql
@@ -1,4 +1,4 @@
---It is important to activate rules after rebuilding Objrefs and importing PL/SQL packages
+--It is important to activate rules and automations after rebuilding Objrefs and importing PL/SQL packages
 --This sample script also validates rules before activation
 
 declare
@@ -11,6 +11,14 @@ declare
           join components_package cp on cp.components_package_id = cpx.components_package_id
          where cpx.component_id = pkg_audit_comp.c_rule_component_id
            and cp.name = p_comp_package;
+    
+    cursor cur_autom_by_comp_package(p_comp_package in components_package.name%type) is
+        select autom.autom_id, autom.autom_name
+          from autom
+          join comp_pkg_object_xref cpx on cpx.object_id = autom.autom_id
+          join components_package cp on cp.components_package_id = cpx.components_package_id
+         where cpx.component_id = pkg_audit_comp.c_autom_component_id
+           and cp.name = p_comp_package;
 begin
     for rec_rule in cur_rules_by_comp_package(c_component_package_name) loop
         dbms_output.put_line('Enabling rule [' ||  rec_rule.rule || ']' || pkg_str.c_lb);
@@ -21,6 +29,14 @@ begin
         
         update rule set is_enabled = 1 where rule_id = rec_rule.rule_id;
         
+        commit;
+    end loop;
+
+    for rec_autom in cur_autom_by_comp_package(c_component_package_name) loop
+        dbms_output.put_line('Ebabling automation [' || rec_autom.autom_name || ']' || pkg_str.c_lb);
+
+        update autom set enabled = 1 where autom_id = rec_autom.autom_id;
+
         commit;
     end loop;
 end;
